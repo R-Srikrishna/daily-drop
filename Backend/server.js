cc1b2cf600b5b21578d37245107bb92d2cdfd394
Your Name <youremail@gmail.com>
2026-03-11 20:57:30 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -22,13 +22,10 @@ app.get("/", (req, res) => {
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://daily-drop-4jpe.vercel.app"
-    ],
+    origin: ["http://localhost:3000", "https://daily-drop-4jpe.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-  })
+    credentials: true,
+  }),
 );
 
 app.use(express.json());
@@ -48,12 +45,11 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT;
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-
   } catch (error) {
     console.log(error);
   }
